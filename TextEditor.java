6910411f42e585d3ea6083e98ebbe4ee648cf659
Atharva Patil <39415890+Attupatil@users.noreply.github.com>
2020-11-15 11:10:24 +0530

    Update TextEditor.java

--- TextEditor.java
+++ TextEditor.java
@@ -1,44 +1,63 @@
-// package editor;
+//package editor;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TextEditor extends JFrame {
     public static void main(final String[] args) {
         new TextEditor();
     }
+    final private JTextArea jTextArea = new JTextArea();
+
     public TextEditor() {
-//        super("TextEditor");
+        super("TextEditor");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
+        setSize(640, 480);
         setLocationRelativeTo(null);
         initComponents();
         setVisible(true);
         setLayout(null);
 
     }
-    
+
     private void initComponents() {
 
 //__________TEXT AREA____________
 
-        JTextArea TextArea = new JTextArea();
-        TextArea.setName("TextArea");
-        setTitle("TextEditor");
-        TextArea.setLineWrap(true);
-        TextArea.setWrapStyleWord(true);
-        // JTextField nameTextField = new JTextField();
-        // nameTextField.setBounds(1,1, 100,30);
-        // add(nameTextField);
+        jTextArea.setName("TextArea");
+        final JScrollPane TextArea = new JScrollPane(jTextArea);
+        TextArea.setName("ScrollPane");
+        TextArea.setBounds(10, 40, super.getWidth() - 25, super.getHeight() / 100 * 90);
+        TextArea.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        TextArea.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         add(TextArea);
 
-//__________ScrollPane____________
-         JScrollPane ScrollPane = new JScrollPane(TextArea);  
-         TextArea.setName("ScrollPane");
-  
-        ScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  
-        ScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);  
-        add(ScrollPane);
-        
+//__________Save & Load Components (Button)____________
+        JButton SaveButton = new JButton("Save");
+        SaveButton.setName("SaveButton");
+        SaveButton.setBounds(250,9,75,30);
+        SaveButton.addActionListener(actionEvent -> {
+           //To be updated
+        });
+        add(SaveButton);
+        JButton LoadButton = new JButton("Load");
+        LoadButton.setName("LoadButton");
+        LoadButton.setBounds(330,9,75,30);
+        LoadButton.addActionListener(actionEvent -> {
+              //To be updated
+        });
+        add(LoadButton);
+        JTextField FilenameField = new JTextField();
+        FilenameField.setName("FilenameField");
+        FilenameField.setBounds(10,10,220,30);
+        add(FilenameField);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBounds(10,10,220,30);
+        greenPanel.setLayout(new BorderLayout());
+        // greenPanel.setBackground(Color.GREEN);
+        add(greenPanel);
+
 
     }
 }
